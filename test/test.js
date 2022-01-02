a9486a053a6b78eddceb6bb8a56e36ea196f5cca
Håkan Olsson <hakanols@gmail.com>
2022-01-02 21:17:09 +0100

    Update js runner, adding test framework and exposing error with logging syntax error

--- test/test.js
+++ test/test.js
@@ -1,5 +1,15 @@
-console.log("All is well in test.js");
-console.log("Log message");
-console.debug("Debug message");
-console.warn("Warning message");
-console.error("Error message");
+import test from './tap-esm.js'
+
+test('Console log', async function (t) {
+    console.log("Log message");
+    console.debug("Debug message");
+    console.warn("Warning message");
+    console.error("Error message");
+	t.end();
+});
+
+test('Syntax error', async function (t) {
+    let empty = null;
+	t.arrayEqual(empty[0], null, "Array index 0 of null, should crash");
+	t.end();
+});
